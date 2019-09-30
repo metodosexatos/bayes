24b72cd4687c2b0432324ce283416183a8e03b1c
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-09-30 12:33:27 -0300

    Update 362_DistribuicaoContinua_Beta.R

--- 362_DistribuicaoContinua_Beta.R
+++ 362_DistribuicaoContinua_Beta.R
@@ -1,12 +1,12 @@
 #********************************************************#
 # www.metodosexatos.com                                  #
-# Prof.Ms. André Santos | andre@metodosexatos.com.br     #
-# Curso Análise Bayesiana com R                          #
-# Distribuições de Probabilidade Contínuas               #
+# Prof.Ms. AndrÃ© Santos | andre@metodosexatos.com.br     #
+# Curso AnÃ¡lise Bayesiana com R                          #
+# DistribuiÃ§Ãµes de Probabilidade ContÃ­nuas               #
 # Data: 28/09/2019                                       #
 #********************************************************#
 
-# Distribuição Beta
+# DistribuiÃ§Ã£o Beta
 
 # Densidade de probabilidade para x = 0,2 em uma Beta(alfa = 3, beta = 10)
 dbeta(0.2, 3, 10)
@@ -14,19 +14,19 @@ dbeta(0.2, 3, 10)
 # Probabilidade acumulada p(X<=0.2)
 pbeta(0.2, 3, 10)
 
-# Probabilidade acumulada p(X>=0.2)
+# Probabilidade acumulada p(X>0.2)
 1-pbeta(0.2, 3, 10)
 
 # Quantil x100q que satisfaz P(X<=x100q) = 0.15
 qbeta(0.15, 3, 10)
 
-# Uma amostra aleatória com 9 elementos
+# Uma amostra aleatÃ³ria com 9 elementos
 a <- 9
 alfa <- 3
 beta <- 10
 Beta <- runif(a, alfa, beta)
 print(Beta)
 
-# Representação gráfica da distribuição Beta
+# RepresentaÃ§Ã£o grÃ¡fica da distribuiÃ§Ã£o Beta
 hist(Beta)
 
