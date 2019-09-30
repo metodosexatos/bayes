09bc1ddad6c292acad303fea91baa167a9c21369
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-09-30 12:34:44 -0300

    Update 365_DistribuicaoContinua_Gama.R

--- 365_DistribuicaoContinua_Gama.R
+++ 365_DistribuicaoContinua_Gama.R
@@ -1,12 +1,12 @@
 #********************************************************#
 # www.metodosexatos.com                                  #
-# Prof.Ms. André Santos | andre@metodosexatos.com.br     #
-# Curso Análise Bayesiana com R                          #
-# Distribuições de Probabilidade Contínuas               #
+# Prof.Ms. AndrÃ© Santos | andre@metodosexatos.com.br     #
+# Curso AnÃ¡lise Bayesiana com R                          #
+# DistribuiÃ§Ãµes de Probabilidade ContÃ­nuas               #
 # Data: 28/09/2019                                       #
 #********************************************************#
 
-# Distribuição Gama
+# DistribuiÃ§Ã£o Gama
 
 # Densidade de probabilidade para x = 5 em uma Gama(alfa = 2, beta = 1)
 dgamma(5, 2, 1)
@@ -14,19 +14,19 @@ dgamma(5, 2, 1)
 # Probabilidade acumulada p(X<=4)
 pgamma(4, 2, 1)
 
-# Probabilidade acumulada p(X>=4)
+# Probabilidade acumulada p(X>4)
 1-pgamma(4, 2, 1)
 
 # Quantil x100q que satisfaz P(X<=x100q) = 0.35
 qgamma(0.15, 2, 1)
 
-# Uma amostra aleatória com 8 elementos
+# Uma amostra aleatÃ³ria com 8 elementos
 a <- 8
 alfa <- 2
 beta <- 1
 gama <- rgamma(a, alfa, beta)
 print(gama)
 
-# Representação gráfica da distribuição Gama
+# RepresentaÃ§Ã£o grÃ¡fica da distribuiÃ§Ã£o Gama
 hist(gama)
 
