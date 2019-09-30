001160093531043278eb9be31d00ec646cb13f5c
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-09-30 12:32:58 -0300

    Update 361_DistribuicaoContinua_U.R

--- 361_DistribuicaoContinua_U.R
+++ 361_DistribuicaoContinua_U.R
@@ -1,12 +1,12 @@
 #********************************************************#
 # www.metodosexatos.com                                  #
-# Prof.Ms. André Santos | andre@metodosexatos.com.br     #
-# Curso Análise Bayesiana com R                          #
-# Distribuições de Probabilidade Contínuas               #
+# Prof.Ms. AndrÃ© Santos | andre@metodosexatos.com.br     #
+# Curso AnÃ¡lise Bayesiana com R                          #
+# DistribuiÃ§Ãµes de Probabilidade ContÃ­nuas               #
 # Data: 28/09/2019                                       #
 #********************************************************#
 
-# Distribuição Uniforme
+# DistribuiÃ§Ã£o Uniforme
 
 # Densidade de probabilidade para x = 3 em uma uniforme U(2,6)
 dunif(3, 2, 6)
@@ -14,19 +14,19 @@ dunif(3, 2, 6)
 # Probabilidade acumulada p(X<=3.5)
 punif(3.5, 2, 6)
 
-# Probabilidade acumulada p(X>=3.5)
+# Probabilidade acumulada p(X>3.5)
 1-punif(3.5, 2, 6)
 
 # Quantil x100q que satisfaz P(X<=x100q) = 0.1
 qunif(0.1, 2, 6)
 
-# Uma amostra aleatória com 9 elementos
+# Uma amostra aleatÃ³ria com 9 elementos
 a <- 9
 c <- 2
 d <- 6
 u <- runif(a, c, d)
 print(u)
 
-# Representação gráfica da distribuição uniforme
+# RepresentaÃ§Ã£o grÃ¡fica da distribuiÃ§Ã£o uniforme
 hist(u)
 
