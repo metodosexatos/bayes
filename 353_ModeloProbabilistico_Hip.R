7539919152de135566d6aed69fe5bb1e653c22f7
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-09-30 12:30:33 -0300

    Update 353_ModeloProbabilistico_Hip.R

--- 353_ModeloProbabilistico_Hip.R
+++ 353_ModeloProbabilistico_Hip.R
@@ -1,12 +1,12 @@
 #********************************************************#
 # www.metodosexatos.com                                  #
-# Prof.Ms. AndrÈ Santos | andre@metodosexatos.com.br     #
-# Curso An·lise Bayesiana com R                          #
-# MODELOS PROBABILÕSTICOS DISCRETOS                      #
+# Prof.Ms. Andr√© Santos | andre@metodosexatos.com.br     #
+# Curso An√°lise Bayesiana com R                          #
+# MODELOS PROBABIL√çSTICOS DISCRETOS                      #
 # Data: 26/09/2019                                       #
 #********************************************************#
 
-# DistribuiÁ„o HypergeomÈtrica
+# Distribui√ß√£o Hypergeom√©trica
 
 # Densidade de probabilidade para x = 2, M = 15, 'N - M' = 30, n = 10
 dhyper(2, 15, 30, 10)
@@ -14,20 +14,20 @@ dhyper(2, 15, 30, 10)
 # Probabilidade acumulada p(X<=3)
 phyper(2, 15, 30, 10)
 
-# Probabilidade acumulada p(X>=3)
+# Probabilidade acumulada p(X>3)
 1-phyper(2, 15, 30, 10)
 
 # Quantil x100q que satisfaz P(X<=x100q) = 0.4
 qhyper(0.4, 15, 30, 10)
 
-# Uma amostra aleatÛria com 12 elementos
+# Uma amostra aleat√≥ria com 12 elementos
 a <- 12
 M <- 15
 NM <- 30
 n <- 10
 Hypergeometrica <- rhyper(a, M, NM, n)
 
-# RepresentaÁ„o gr·fica da distribuiÁ„o hypergeometrica
+# Representa√ß√£o gr√°fica da distribui√ß√£o hypergeometrica
 hist(Hypergeometrica)
 
 
